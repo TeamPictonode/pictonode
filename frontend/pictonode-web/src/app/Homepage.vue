925e6fcdacf2234794f50a219dd84f9791569209
jtnunley <jtnunley01@gmail.com>
2022-12-08 07:50:51 -0800

    Fix webpack build error that happens irregularly

--- frontend/pictonode-web/src/app/Homepage.vue
+++ frontend/pictonode-web/src/app/Homepage.vue
@@ -1,13 +1,5 @@
 <!-- GNU AGPL v3 License -->
 
-<script lang="ts">
-import { defineComponent } from "vue";
-
-export default defineComponent({
-  name: "Homepage",
-});
-</script>
-
 <template>
   <div id="homepage">
     <p class="text-h5 text-center">Welcome to Pictonode!</p>
