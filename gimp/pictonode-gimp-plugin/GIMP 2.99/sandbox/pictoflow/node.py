34eca21ce6a82a1e01ebe72158401e5079fcf8ee
Stephen Foster <stephenfostercpp@gmail.com>
2023-01-03 10:50:29 -0800

    Will be squashed

---
+++ gimp/pictonode-gimp-plugin/GIMP 2.99/sandbox/pictoflow/node.py
@@ -0,0 +1,10 @@
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+gi.require_version('Gdk', '3.0')
+from gi.repository import Gdk
+
+class Node(Gtk.Box):
+    def __init__(self):
+        #self.widget
+        pass
