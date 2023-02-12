7ea1cbc06687968a9ba2d9c3c05cc0a738d757be
jtnunley <jtnunley01@gmail.com>
2023-02-12 08:01:24 -0800

    Add more image ops to ontario

--- backend/ontario-web/ontario-web/__init__.py
+++ backend/ontario-web/ontario-web/__init__.py
@@ -5,31 +5,32 @@ import os
 
 from flask import Flask
 
-def create_app(test_config = None):
-  # Create and configure the app
-  app = Flask(__name__, instance_relative_config = True)
-  app.config.from_mapping(
-    SECRET_KEY="ontario",
-    # TODO: not sqlite
-    DATABASE=os.path.join(app.instance_path, "ontario.sqlite"),
-  )
 
-  if test_config is None:
-    # Load the instance config, if it exists, when not testing
-    app.config.from_pyfile("config.py", silent = True)
-  else:
-    # Load the test config if passed in
-    app.config.from_mapping(test_config)
-  
-  # Ensure the instance folder exists
-  try:
-    os.makedirs(app.instance_path)
-  except OSError:
-    pass
+def create_app(test_config=None):
+    # Create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY="ontario",
+        # TODO: not sqlite
+        DATABASE=os.path.join(app.instance_path, "ontario.sqlite"),
+    )
 
-  # Host all files in the "public" folder
-  @app.route("/<path:path>")
-  def send_file(path):
-    return app.send_static_file(path)
-  
-  return app
+    if test_config is None:
+        # Load the instance config, if it exists, when not testing
+        app.config.from_pyfile("config.py", silent=True)
+    else:
+        # Load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # Ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # Host all files in the "public" folder
+    @app.route("/<path:path>")
+    def send_file(path):
+        return app.send_static_file(path)
+
+    return app
---
+++ backend/ontario/tests/test_smoke.py
@@ -0,0 +1,152 @@
+# GNU AGPL v3 License
+# Written by John Nunley
+
+import ontario
+import os.path as path
+import pytest
+import tempfile
+
+TEST_IMAGE_PATH = path.join(path.dirname(__file__), "assets", "test.png")
+
+
+@pytest.fixture
+def test_load_from_file():
+    """
+    Tests the load_from_file method.
+    """
+
+    context = ontario.ImageContext()
+    builder = ontario.ImageBuilder(context)
+    builder.load_from_file(TEST_IMAGE_PATH)
+    builder.process()
+
+
+@pytest.fixture
+def test_save_to_file():
+    """
+    Tests the save_to_file method.
+    """
+
+    with tempfile.TemporaryFile("w+") as file:
+        context = ontario.ImageContext()
+        builder = ontario.ImageBuilder(context)
+        builder.load_from_file(TEST_IMAGE_PATH)
+        builder.save_to_file(file)
+        builder.process()
+
+
+@pytest.fixture
+def test_composite():
+    """
+    Tests the composite method.
+    """
+
+    context = ontario.ImageContext()
+    builder = ontario.ImageBuilder(context)
+    builder.load_from_file(TEST_IMAGE_PATH)
+    builder.composite(builder.load_from_file(TEST_IMAGE_PATH))
+    builder.process()
+
+
+@pytest.fixture
+def test_invert():
+    """
+    Tests the invert method.
+    """
+
+    context = ontario.ImageContext()
+    builder = ontario.ImageBuilder(context)
+    builder.load_from_file(TEST_IMAGE_PATH)
+    builder.invert()
+    builder.process()
+
+
+@pytest.fixture
+def test_crop():
+    """
+    Tests the crop method.
+    """
+
+    context = ontario.ImageContext()
+    builder = ontario.ImageBuilder(context)
+    builder.load_from_file(TEST_IMAGE_PATH)
+    builder.crop(0, 0, 100, 100)
+    builder.process()
+
+
+@pytest.fixture
+def test_flip():
+    """
+    Tests the flip method.
+    """
+
+    context = ontario.ImageContext()
+    builder = ontario.ImageBuilder(context)
+    builder.load_from_file(TEST_IMAGE_PATH)
+    builder.flip()
+    builder.process()
+
+
+@pytest.fixture
+def test_rotate():
+    """
+    Tests the rotate method.
+    """
+
+    context = ontario.ImageContext()
+    builder = ontario.ImageBuilder(context)
+    builder.load_from_file(TEST_IMAGE_PATH)
+    builder.rotate(90)
+    builder.process()
+
+
+@pytest.fixture
+def test_color_balance():
+    """
+    Tests the color_balance method.
+    """
+
+    context = ontario.ImageContext()
+    builder = ontario.ImageBuilder(context)
+    builder.load_from_file(TEST_IMAGE_PATH)
+    builder.color_balance(0.5, 0.5, 0.5)
+    builder.process()
+
+
+@pytest.fixture
+def test_hue_saturation():
+    """
+    Tests the hue_saturation method.
+    """
+
+    context = ontario.ImageContext()
+    builder = ontario.ImageBuilder(context)
+    builder.load_from_file(TEST_IMAGE_PATH)
+    builder.hue_saturation(0.5, 0.5)
+    builder.process()
+
+
+@pytest.fixture
+def test_unsharp_mask():
+    """
+    Tests the unsharp_mask method.
+    """
+
+    context = ontario.ImageContext()
+    builder = ontario.ImageBuilder(context)
+    builder.load_from_file(TEST_IMAGE_PATH)
+    builder.unsharp_mask(0.5, 0.5)
+    builder.process()
+
+
+@pytest.fixture
+def test_guassian_blur():
+    """
+    Tests the guassian_blur method.
+    """
+
+    context = ontario.ImageContext()
+    builder = ontario.ImageBuilder(context)
+    builder.load_from_file(TEST_IMAGE_PATH)
+    builder.guassian_blur(0.5)
+    builder.process()
