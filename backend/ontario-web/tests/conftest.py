b80882e0c04261f96c0d02b474a80202fb171c48
John Nunley <jtnunley01@gmail.com>
2023-02-26 12:23:14 -0800

    Add login and register API (#71)
    * Add login and register API
    
    * Setup DB test harness
    
    * Set up testing db
    
    * Make the tests pass

--- backend/ontario-web/tests/conftest.py
+++ backend/ontario-web/tests/conftest.py
@@ -3,15 +3,39 @@
 
 import os
 import pytest
+import psycopg2
 import tempfile
 
+from os import path
+
 from ontario_web import create_app
+from ontario_web.db import get_db, init_db
 
 @pytest.fixture
 def app():
-    app = create_app({
+    test_config = {
       'TESTING': True,
-    })
+      'DATABASE': 'ontario_test',
+      'USER': 'ontario_test',
+      'PASSWORD': 'ontario_test',
+      'HOST': 'localhost',
+      'PORT': '5432'
+    }
+    app = create_app(test_config)
+
+    with app.app_context():
+        init_db(test_config)
+        with open(path.join(path.dirname(__file__), "setup.sql"), "r") as f:
+            conn = get_db()
+            cursor = conn.cursor()
+            try:
+              cursor.execute(f.read())
+              conn.commit()
+            except psycopg2.ProgrammingError:
+              pass
+            finally:
+              cursor.close()
+              conn.close()
 
     yield app
 
