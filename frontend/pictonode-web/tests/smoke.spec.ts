aa86027ac1d88ce6454c4cc2bf4df8e8d47c2870
Grace Meredith <gmeredith@nevada.unr.edu>
2023-03-02 20:25:31 -0800

    temp

--- frontend/pictonode-web/tests/smoke.spec.ts
+++ frontend/pictonode-web/tests/smoke.spec.ts
@@ -1,8 +1,12 @@
-import Flow from "../src/components/nodes/Flow.vue";
+import { DataType } from "../src/components/nodes/NodeTypes"
 import { expect } from "chai";
 
 describe("Smoke Test", () => {
-  it("should return a pipeline", () => {
-    
+  it("should return a DataType", () => {
+   const color = DataType.Color
+   const image = DataType.Image
+
+   expect(color).to.eq("color")
+   expect(image).to.eq("image")
   });
 })
