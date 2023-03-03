0573269f0439cabbf82a6db375f6f9900d17927d
Stephen Foster <67705843+Stehfyn@users.noreply.github.com>
2023-03-02 17:54:40 -0800

    Removed deprecated imports and functions

--- gimp/pictonode-gimp-plugin/GIMP 2.99/pictonode/pictonode.py
+++ gimp/pictonode-gimp-plugin/GIMP 2.99/pictonode/pictonode.py
@@ -1,35 +1,13 @@
 #!/usr/bin/env python3
 
 # This file was written in its entirety by Parker Nelms and Stephen Foster.
-
-from httpclient import *
-from client import *
-from manager import PictonodeManager
-import window
-
-import sys
-import threading
 import os
+import sys
+
+from manager import PictonodeManager
 
 # autopep8 off
 import gi
-gi.require_version("GIRepository", "2.0")
-from gi.repository import GIRepository  # noqa
-
-GIRepository.Repository.prepend_search_path(
-    os.path.realpath(
-        os.path.dirname(
-            os.path.abspath(__file__)) +
-        "/introspection"))
-
-GIRepository.Repository.prepend_library_path(
-    os.path.realpath(
-        os.path.dirname(
-            os.path.abspath(__file__)) +
-        "/libs"))
-
-gi.require_version("GtkNodes", "0.1")
-from gi.repository import GtkNodes  # noqa
 
 gi.require_version('Gimp', '3.0')
 from gi.repository import Gimp  # noqa
@@ -37,67 +15,12 @@ from gi.repository import Gimp  # noqa
 gi.require_version('GimpUi', '3.0')
 from gi.repository import GimpUi  # noqa
 
-gi.require_version('Gegl', '0.4')
-from gi.repository import Gegl  # noqa
-
-gi.require_version("Gio", "2.0")
-from gi.repository import Gio  # noqa
-
 gi.require_version("GLib", "2.0")
 from gi.repository import GLib  # noqa
-
-gi.require_version("GObject", "2.0")
-from gi.repository import GObject  # noqa
 # autopep8 on
 
-
 def N_(message): return message
-
-
 def _(message): return GLib.dgettext(None, message)
-
-
-def send_image_to_daemon(button, gegl):
-
-    def do_send_image(gegl):
-        image = save_layer_to_png(gegl)
-        http_client.send_image(image)
-
-    x = threading.Thread(target=do_send_image, args=(gegl,))
-    x.start()
-    x.join()
-
-
-def save_layer_to_png(gegl_buffer):
-    STATIC_TARGET_DIR = os.path.abspath(
-        f"{os.path.dirname(os.path.abspath(__file__))}\\int")
-    STATIC_TARGET = os.path.abspath(
-        f"{STATIC_TARGET_DIR}\\pictonode-intermediate.png")
-
-    # make the empty target
-    os.makedirs(STATIC_TARGET_DIR, exist_ok=True)
-    intermediate_file = os.open(STATIC_TARGET, os.O_CREAT | os.O_TRUNC)
-    os.close(intermediate_file)
-
-    Gegl.init(None)
-    parent = Gegl.Node()
-
-    buffer_input = Gegl.Node()
-    buffer_input.set_property("operation", "gegl:buffer-source")
-    buffer_input.set_property("buffer", gegl_buffer)
-    parent.add_child(buffer_input)
-
-    buffer_output = Gegl.Node()
-    buffer_output.set_property("operation", "gegl:png-save")
-    buffer_output.set_property("path", STATIC_TARGET)
-    parent.add_child(buffer_output)
-
-    buffer_input.connect_to("output", buffer_output, "input")
-
-    buffer_output.process()
-
-    return STATIC_TARGET
-
 
 def register_instance_parasite() -> None:
     from utils import get_pid_timestamp
