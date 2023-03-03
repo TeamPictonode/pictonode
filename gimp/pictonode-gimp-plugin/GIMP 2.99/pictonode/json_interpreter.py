612a0dfad249cba828789d25becf2025137dbaae
Parker <parkern342@gmail.com>
2023-03-02 19:02:50 -0800

    node position bug fix

--- gimp/pictonode-gimp-plugin/GIMP 2.99/pictonode/json_interpreter.py
+++ gimp/pictonode-gimp-plugin/GIMP 2.99/pictonode/json_interpreter.py
@@ -9,20 +9,32 @@ def json_interpreter(node_view, json_string: dict, window):
 
     instantiation_dict = {'SrcNode': cn.ImgSrcNode, 'OutNode': cn.OutputNode, 'InvertNode': cn.InvertNode, 'BlurNode': cn.BlurNode}
 
+    nodes_loaded = {}
+
     for node in json_string["nodes"]:
 
         node_str = node["template"]
-        x_str = node["metadata"]["x"]
-        y_str = node["metadata"]["y"] // 2
+        x = node["metadata"]["x"]
+        y = node["metadata"]["y"]
+
+        print("x: ", x)
+        print("y: ", y)
 
         if node_str in instantiation_dict:
             # add node at given position
             node_obj = instantiation_dict[node_str](window)
             node_view.add(node_obj)
-            node_view.child_set_property(node_obj, "x", x_str)
-            node_view.child_set_property(node_obj, "y", y_str)
+            node_obj.set_property("x", x)
+            node_obj.set_property("y", y)
+
+            nodes_loaded[node["id"]] = node_obj
+            # node_view.child_set_property(node_obj, "x", x)
+            # node_view.child_set_property(node_obj, "y", y)
 
         else:
             return False
+        
+    for link in json_string["links"]:
+        pass
 
     return True
