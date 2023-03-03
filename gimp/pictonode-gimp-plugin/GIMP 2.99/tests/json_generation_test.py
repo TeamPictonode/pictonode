522a8227780a9673c5d27a2195a1f7959b32ff6b
parkern342 <parkern342@gmail.com>
2023-03-03 01:01:23 -0800

    clean up

--- gimp/pictonode-gimp-plugin/GIMP 2.99/tests/json_generation_test.py
+++ gimp/pictonode-gimp-plugin/GIMP 2.99/tests/json_generation_test.py
@@ -35,14 +35,19 @@ from gi.repository import Gegl  # noqa
 class TestJSONGenerator(unittest.TestCase):
 
     def test_json_generator_output(self):
+        # import needed modules
         from pictonode import json_generator, custom_nodes
+
+        # create empty NodeView
         node_view = GtkNodes.NodeView()
 
+        # add nodes to NodeView
         node_view.add(custom_nodes.OutputNode(node_window=None))
         node_view.add(custom_nodes.CompositeNode(node_window=None))
         node_view.add(custom_nodes.InvertNode(node_window=None))
         node_view.add(custom_nodes.BlurNode(node_window=None))
 
+        # set basis for correct json representation
         json_true = {'nodes':
                      [{'id': 0,
                        'template': 'OutNode',
@@ -71,8 +76,10 @@ class TestJSONGenerator(unittest.TestCase):
                      'links': [],
                      'output': 0}
 
+        # generate json represention given NodeView
         json_result = json_generator.serialize_nodes(node_view=node_view)
 
+        # compare results using the python unittest library/objects
         self.assertEqual(json_result, json_true)
 
 if __name__ == '__main__':
