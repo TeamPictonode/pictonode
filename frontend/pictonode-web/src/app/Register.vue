6c2904839cf9421643e6722cc6319b12ab4ec326
Grace Meredith <gmeredith@nevada.unr.edu>
2023-03-06 21:48:07 -0800

    add methods

--- frontend/pictonode-web/src/app/Register.vue
+++ frontend/pictonode-web/src/app/Register.vue
@@ -8,7 +8,32 @@
 import { defineComponent } from "vue";
 
 export default defineComponent({
+  data: () => ({
+    username: null as string | null,
+    email: null as string | null,
+    firstName: null as string | null,
+    lastName: null as string | null,
+    password: null as string | null,
+  }),
   name: "Register",
+
+  methods:{
+    addUsername(template: string) {
+      this.username = template
+    },
+    addEmail(template: string) {
+      this.email = template
+    },
+    addFirstName(template: string) {
+      this.firstName = template
+    },
+    addLastName(template: string) {
+      this.lastName = template
+    },
+    setPassword(template: string) {
+      this.password = template
+    }
+  }
 });
 </script>
 
@@ -20,28 +45,28 @@ export default defineComponent({
         <v-container>
           <v-row>
             <v-col cols="12">
-              <v-text-field label="Username" solo />
+              <v-text-field label="Username" solo @input="addUsername"/>
             </v-col>
           </v-row>
           <v-row>
             <v-col cols="12">
-              <v-text-field label="Email" solo />
+              <v-text-field label="Email" solo @input="addEmail"/>
             </v-col>
           </v-row>
           <v-row>
             <v-col cols="12" sm="6">
-              <v-text-field label="First Name" solo />
+              <v-text-field label="First Name" solo @input="addFirstName"/>
             </v-col>
             <v-col cols="12" sm="6">
-              <v-text-field label="Last Name" solo />
+              <v-text-field label="Last Name" solo @input="addLastName"/>
             </v-col>
           </v-row>
           <v-row>
             <v-col cols="6">
-              <v-text-field type="password" label="Password" />
+              <v-text-field type="password" label="Password" @input="setPassword"/>
             </v-col>
             <v-col cols="6">
-              <v-text-field type="password" label="Confirm Password" />
+              <v-text-field type="password" label="Confirm Password" @input="confirmPassword"/>
             </v-col>
           </v-row>
         </v-container>
@@ -49,7 +74,7 @@ export default defineComponent({
     </v-card-text>
     <v-card-actions>
       <v-spacer />
-      <v-btn color="primary">Register</v-btn>
+      <v-btn color="primary" @input="setRegister">Register</v-btn>
     </v-card-actions>
   </v-card>
 </template>
