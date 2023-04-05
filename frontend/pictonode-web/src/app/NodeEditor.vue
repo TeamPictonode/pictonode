e3d2f06924c6d0c76e0b455ec0e8a43b49d2f62f
jtnunley <jtnunley01@gmail.com>
2023-04-05 10:35:52 -0700

    Use a real error icon

--- frontend/pictonode-web/src/app/NodeEditor.vue
+++ frontend/pictonode-web/src/app/NodeEditor.vue
@@ -105,40 +105,58 @@ export default defineComponent({
           };
         })
         .catch((err) => {
-          // Instead make a canvas that says "no image".
-          const canvas = document.createElement("canvas");
-          canvas.width = 800;
-          canvas.height = 600;
-          const ctx = canvas.getContext("2d");
+          // Try to load the file at "/error_icon.png" and use that.
+          const image = new Image();
+          image.src = "/error_icon.png";
 
-          if (ctx) {
-            // Draw a black background.
-            ctx.fillStyle = "black";
-            ctx.fillRect(0, 0, 800, 600);
+          image.onload = () => {
+            // Cast the image into a canvas.
+            const canvas = document.createElement("canvas");
+            canvas.width = image.width;
+            canvas.height = image.height;
+            const ctx = canvas.getContext("2d");
+            if (ctx) {
+              ctx.drawImage(image, 0, 0);
+              this.img = canvas;
+            }
+          };
 
-            // Draw the text "NO IMAGE"
-            ctx.font = "60px Arial";
-            ctx.fillStyle = "white";
-            ctx.fillText("NO IMAGE", 100, 50);
+          image.onerror = () => {
+            // Instead make a canvas that says "no image".
+            const canvas = document.createElement("canvas");
+            canvas.width = 800;
+            canvas.height = 600;
+            const ctx = canvas.getContext("2d");
 
-            // Draw a frowny face.
-            ctx.strokeStyle = "white";
-            ctx.beginPath();
-            ctx.arc(400, 300, 100, 0, 2 * Math.PI);
-            ctx.stroke();
-            ctx.beginPath();
-            ctx.arc(350, 250, 10, 0, 2 * Math.PI);
-            ctx.stroke();
-            ctx.beginPath();
-            ctx.arc(450, 250, 10, 0, 2 * Math.PI);
-            ctx.stroke();
-            ctx.beginPath();
-            ctx.moveTo(350, 350);
-            ctx.lineTo(450, 350);
-            ctx.stroke();
+            if (ctx) {
+              // Draw a black background.
+              ctx.fillStyle = "black";
+              ctx.fillRect(0, 0, 800, 600);
 
-            this.img = canvas;
-          }
+              // Draw the text "NO IMAGE"
+              ctx.font = "60px Arial";
+              ctx.fillStyle = "white";
+              ctx.fillText("NO IMAGE", 100, 50);
+
+              // Draw a frowny face.
+              ctx.strokeStyle = "white";
+              ctx.beginPath();
+              ctx.arc(400, 300, 100, 0, 2 * Math.PI);
+              ctx.stroke();
+              ctx.beginPath();
+              ctx.arc(350, 250, 10, 0, 2 * Math.PI);
+              ctx.stroke();
+              ctx.beginPath();
+              ctx.arc(450, 250, 10, 0, 2 * Math.PI);
+              ctx.stroke();
+              ctx.beginPath();
+              ctx.moveTo(350, 350);
+              ctx.lineTo(450, 350);
+              ctx.stroke();
+
+              this.img = canvas;
+            }
+          };
         });
     },
   },
